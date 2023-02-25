31276616c4c31bc0c3c1bf059cc311c65d8e3af8
sheep <micahanichols27@gmail.com>
2023-02-25 15:48:01 -0600

    Revert "assn4: temp connection test"
    This reverts commit 9f43d69d1656b2f7c85f7e0c68f1aafe6ee1c686.

--- findOrdersState.php
+++ findOrdersState.php
@@ -9,7 +9,7 @@ or die('Error connecting to MySQL server.');
 
 <html>
 <head>
-  <title>Another Simple PHP-MySQL Program</title>
+  <title>Manufacturer Query</title>
   </head>
   
   <body bgcolor="white">
@@ -28,6 +28,15 @@ $state = mysqli_real_escape_string($conn, $state);
 
 $query = "SELECT DISTINCT firstName, lastName, city FROM customer WHERE state = ";
 $query = $query."'".$state."' ORDER BY 2;";
+
+$query = "SELECT DISTINCT c.fname, c.lname, s.description ";
+$query = $query."FROM customer c ";
+$query = $query."JOIN orders o USING (customer_num) ";
+$query = $query."JOIN items i USING (order_num) ";
+$query = $query."JOIN stock s USING (stock_num) ";
+$query = $query."JOIN manufact m ON (s.manu_code=m.manu_code) ";
+$query = $query."WHERE m.manu_name = "."'".$state."' ";
+$query = $query."ORDER BY c.lname;";
 
 ?>
 
@@ -51,7 +60,7 @@ print "<pre>";
 while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
   {
     print "\n";
-    print "$row[firstName]  $row[lastName]  $row[city]";
+    print "$row[fname]  $row[lname]  $row[description]";
   }
 print "</pre>";
 
@@ -65,7 +74,7 @@ mysqli_close($conn);
 <hr>
 
 <p>
-<a href="findCustState.txt" >Contents</a>
+<a href="findOrdersState.txt" >Contents</a>
 of the PHP program that created this page. 	 
  
 </body>
