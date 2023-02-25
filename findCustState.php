9f43d69d1656b2f7c85f7e0c68f1aafe6ee1c686
sheep <micahanichols27@gmail.com>
2023-02-25 15:43:43 -0600

    assn4: temp connection test
    assn4: temp connection test

--- findCustState.php
+++ findCustState.php
@@ -9,7 +9,7 @@ or die('Error connecting to MySQL server.');
 
 <html>
 <head>
-  <title>Manufacturer Query</title>
+  <title>Another Simple PHP-MySQL Program</title>
   </head>
   
   <body bgcolor="white">
@@ -28,15 +28,6 @@ $state = mysqli_real_escape_string($conn, $state);
 
 $query = "SELECT DISTINCT firstName, lastName, city FROM customer WHERE state = ";
 $query = $query."'".$state."' ORDER BY 2;";
-
-$query = "SELECT DISTINCT c.fname, c.lname, s.description ";
-$query = $query."FROM customer c ";
-$query = $query."JOIN orders o USING (customer_num) ";
-$query = $query."JOIN items i USING (order_num) ";
-$query = $query."JOIN stock s USING (stock_num) ";
-$query = $query."JOIN manufact m ON (s.manu_code=m.manu_code) ";
-$query = $query."WHERE m.manu_name = "."'".$state."' ";
-$query = $query."ORDER BY c.lname;";
 
 ?>
 
@@ -60,7 +51,7 @@ print "<pre>";
 while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
   {
     print "\n";
-    print "$row[fname]  $row[lname]  $row[description]";
+    print "$row[firstName]  $row[lastName]  $row[city]";
   }
 print "</pre>";
 
@@ -74,7 +65,7 @@ mysqli_close($conn);
 <hr>
 
 <p>
-<a href="findOrdersState.txt" >Contents</a>
+<a href="findCustState.txt" >Contents</a>
 of the PHP program that created this page. 	 
  
 </body>
